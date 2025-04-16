628de59837dc480c66e19bc2a7b7942ae3ccce5a
joaogitdb <joao-paulo.realista.ext@inetum.com>
2025-04-16 09:48:39 +0200

    Ejercicio 4 configuracion funcional

---
+++ soap-service/src/test/java/com/example/soap_service/SoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soap_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
