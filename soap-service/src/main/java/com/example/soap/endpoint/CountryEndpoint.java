a5028a11cd11ea8b0b6f9c73cf9efb34e984c85a
joaogitdb <joao-paulo.realista.ext@inetum.com>
2025-04-16 10:08:53 +0200

    Commit con gen de SOAP

---
+++ soap-service/src/main/java/com/example/soap/endpoint/CountryEndpoint.java
@@ -0,0 +1,33 @@
+package com.example.soap.endpoint;
+
+import com.ejemplo.soap.gen.GetCountryRequest;
+import com.ejemplo.soap.gen.GetCountryResponse;
+import com.example.soap.repository.CountryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+
+    private static final String NAMESPACE_URI = "http://www.baeldung.com/springsoap/gen";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        // El repositorio devuelve el país basado en el nombre.
+        response.setCountry(countryRepository.findCountry(request.getName()));
+        return response;
+    }
+}
