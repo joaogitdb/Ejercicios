a8f0d52d765a14136f5cf4103c5075c188cab29f
joaogitdb <joao-paulo.realista.ext@inetum.com>
2025-04-17 09:35:25 +0200

    Commit base del ejercicio 6 (en IDE eclipse)

---
+++ eclipselink-crud/src/main/java/com/empresa/eclipselinkcrud/EclipselinkCrudApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.eclipselinkcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipselinkCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclipselinkCrudApplication.class, args);
+	}
+
+}
---
+++ eclipselink-crud/src/test/java/com/empresa/eclipselinkcrud/EclipselinkCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.eclipselinkcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipselinkCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
